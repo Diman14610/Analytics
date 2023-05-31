faf523670263150263781e2ec39655a1fdf69466
Infinity <n.dima.1998@mail.ru>
2023-05-31 21:02:04 +0300

    Changed the TestApp code of the console

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -2,6 +2,8 @@
 using Analytics.Core;
 using Analytics.Handlers;
 using Analytics.Methods;
+using Analytics.Shared;
+using System.Collections.Concurrent;
 
 namespace TestApp
 {
@@ -9,12 +11,19 @@ namespace TestApp
     {
         static void Main(string[] args)
         {
-            var s1 = new AnalyticsFactory(new HandlersManager(new MethodsManager()));
-            var t2 = s1
+            var s1 = new AnalyticsFactory(new HandlersManager(new MethodsManager()))
                 .CheckFor(r => r.Imsi, r => r.Hex, r => r.Str)
                 .EqualsTo(r => r.Hex)
                 .EqualsTo(r => r.Int)
-                .Analysis("#fff");
+                .EqualsTo(r => r.Ip, r => r.Str);
+
+            var asdsad = new ConcurrentBag<AnalyticsResult>();
+            new string[] { "192.168.20.1", "#ff", "----" }.AsParallel().ForAll(a =>
+            {
+                Thread.Sleep(1000);
+                asdsad.Add(s1.Analysis(a));
+                Console.WriteLine(a);
+            });
 
             Console.ReadKey();
         }
