5e909a202bd1786cb845a2eedbbbf3c43e522ecb
Infinity <n.dima.1998@mail.ru>
2023-06-25 02:30:27 +0300

    Made a temporary type list for the returned type

---
+++ Analytics.Shared/MethodsFactoryStruct.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analytics.Shared
+{
+    public class MethodsFactoryStruct
+    {
+        public IList<TextFactoryMethodInfo> TextFactoryMethod { get; set; } = new List<TextFactoryMethodInfo>();
+
+        public IList<MajorFactoryMethodInfo> MajorFactoryMethod { get; set; } = new List<MajorFactoryMethodInfo>();
+    }
+}
