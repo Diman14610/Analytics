a64928237b998ca3f4b71f68819a0571ca361e3e
Infinity <User@>
2023-06-26 12:11:08 +0300

    Deleted an extra file

--- Analytics.Shared/MethodInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Analytics.Shared
-{
-    public class MethodInfo : BaseResult
-    {
-        public string? MethodName { get; set; }
-
-        public bool IsMethodFound { get; set; }
-    }
-}
