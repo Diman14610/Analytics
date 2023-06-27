5ed22bcdafd038d14e93fe2d76c564b42a2056c1
Infinity <n.dima.1998@mail.ru>
2023-06-27 23:30:37 +0300

    Added a custom method to the construction configuration

--- Analytics.Configuration/AnalyticsConfiguration.cs
+++ Analytics.Configuration/AnalyticsConfiguration.cs
@@ -2,6 +2,24 @@
 {
     public class AnalyticsConfiguration
     {
+        private readonly ICollection<CustomMethodsBuilder> _customMethodsBuilders;
 
+        public AnalyticsConfiguration()
+        {
+            _customMethodsBuilders = new List<CustomMethodsBuilder>();
+        }
+
+        public void AddMethod(CustomMethodsBuilder method)
+        {
+            _customMethodsBuilders.Add(method);
+        }
+
+        public void AddMethods(IEnumerable<CustomMethodsBuilder> methods)
+        {
+            foreach (var method in methods)
+            {
+                AddMethod(method);
+            }
+        }
     }
 }
