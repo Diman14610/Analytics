4fb7caa9ee52acb523f03614be02e4f6fbaf8a7e
Infinity <n.diman1998@gmail.com>
2023-10-23 19:39:38 +0300

    Combined the code for CustomMethods

--- Analytics.Configuration/AnalyticsConfiguration.cs
+++ Analytics.Configuration/AnalyticsConfiguration.cs
@@ -4,31 +4,24 @@ namespace Analytics.Configuration
 {
     public class AnalyticsConfiguration
     {
-        private readonly List<CustomMethod> _customMethods;
+        protected List<CustomMethod> CustomMethods { get; set; } = new List<CustomMethod>();
 
         protected object? Settings { get; set; }
 
-        protected IReadOnlyList<CustomMethod> CustomMethods => _customMethods.AsReadOnly();
-
-        public AnalyticsConfiguration()
-        {
-            _customMethods = new List<CustomMethod>();
-        }
-
         public void AddMethod(CustomMethod method)
         {
-            _customMethods.Add(method);
+            CustomMethods.Add(method);
         }
 
         public void AddMethods(IEnumerable<CustomMethod> methods)
         {
-            _customMethods.AddRange(methods);
+            CustomMethods.AddRange(methods);
         }
 
         public void ApplyConfiguration(AnalyticsConfiguration other)
         {
             Settings = other.Settings;
-            _customMethods.AddRange(other.CustomMethods);
+            CustomMethods.AddRange(other.CustomMethods);
         }
     }
 }
